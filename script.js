e6556959436af13370cdd2d89293c93ad71508d0
cyril jardin <cyilroc60@hotmail.fr>
2018-06-22 10:02:55 +0200

    js8

---
+++ script.js
@@ -0,0 +1,11 @@
+//Déclaration d'une fonction
+function ages(){
+//Déclaration d'une variable
+var age = document.getElementById('age').value;
+//Boite de dialogue avec appel d'une variables
+if (age >= 18){
+   alert('vous êtes majeur');
+}else{
+   alert('vous êtes mineur');
+}
+}
